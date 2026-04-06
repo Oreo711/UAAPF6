11835e00d8f776cce1ed6537acfd7dc5d1f44e6f
Oreo711 <tannerwilkins@gmail.com>
2026-04-06 12:07:31 +0300

    Initial

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Infrastructure/GameplayContextRegistrations.cs
@@ -0,0 +1,68 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Gameplay.Features.AI;
+using Assets._Project.Develop.Runtime.Gameplay.Features.InputFeature;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
+{
+    public class GameplayContextRegistrations
+    {
+        public static void Process(DIContainer container, GameplayInputArgs args)
+        {
+            container.RegisterAsSingle(CreateEntitiesFactory);
+
+            container.RegisterAsSingle(CreateEntitiesLifeContext);
+
+            container.RegisterAsSingle(CreateCollidersRegistryService);
+
+            container.RegisterAsSingle(CreateBrainsFactory);
+
+            container.RegisterAsSingle(CreateAIBrainsContext);
+
+            container.RegisterAsSingle<IInputService>(CreateDesktopInput);
+
+            container.RegisterAsSingle(CreateMonoEntitiesFactory).NonLazy();
+        }
+
+        private static DesktopInput CreateDesktopInput(DIContainer c)
+        {
+            return new DesktopInput();
+        }
+
+        private static AIBrainsContext CreateAIBrainsContext(DIContainer c)
+        {
+            return new AIBrainsContext();
+        }
+
+        private static BrainsFactory CreateBrainsFactory(DIContainer c)
+        {
+            return new BrainsFactory(c);
+        }
+
+        private static CollidersRegistryService CreateCollidersRegistryService(DIContainer c)
+        {
+            return new CollidersRegistryService();
+        }
+
+        private static MonoEntitiesFactory CreateMonoEntitiesFactory(DIContainer c)
+        {
+            return new MonoEntitiesFactory(
+                c.Resolve<ResourcesAssetsLoader>(),
+                c.Resolve<EntitiesLifeContext>(),
+                c.Resolve<CollidersRegistryService>());
+        }
+
+        private static EntitiesLifeContext CreateEntitiesLifeContext(DIContainer c)
+        {
+            return new EntitiesLifeContext();
+        }
+
+        private static EntitiesFactory CreateEntitiesFactory(DIContainer c)
+        {
+            return new EntitiesFactory(c);
+        }
+    }
+}
